8ec622554aef1e781abbe359d17ed852f8564be3
jamiewang76 <57398429+jamiewang76@users.noreply.github.com>
2020-02-25 17:34:40 +0800

    Trace & Skeleton Display

--- src/sketch.js
+++ src/sketch.js
@@ -1,5 +1,6 @@
 // setup initializes this to a p5.js Video instance.
 let video;
+let lineScore;
 
 // p5js calls this code once when the page is loaded (and, during development,
 // when the code is modified.)
@@ -28,12 +29,13 @@ export function setup() {
 export function draw() {}
 
 function drawPoses(poses) {
+  background(0,0,0,20)
   // Modify the graphics context to flip all remaining drawing horizontally.
   // This makes the image act like a mirror (reversing left and right); this is
   // easier to work with.
   translate(width, 0); // move the left side of the image to the right
   scale(-1.0, 1.0);
-  image(video, 0, 0, video.width, video.height);
+  //image(video, 0, 0, video.width, video.height);
   drawKeypoints(poses);
   drawSkeleton(poses);
 }
@@ -43,12 +45,14 @@ function drawKeypoints(poses) {
   poses.forEach(pose =>
     pose.pose.keypoints.forEach(keypoint => {
       if (keypoint.score > 0.2) {
-        fill(0, 255, 0);
+        fill(255, 0, 0);
         noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 20);
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
       }
     })
   );
+
+
 }
 
 // Draw connections between the skeleton joints.
@@ -57,7 +61,10 @@ function drawSkeleton(poses) {
     pose.skeleton.forEach(skeleton => {
       // skeleton is an array of two keypoints. Extract the keypoints.
       const [p1, p2] = skeleton;
-      stroke(255, 0, 0);
+      //changing the stroke color and weight according to the line confidence
+      lineScore=skeleton[0].score*skeleton[1].score
+      stroke(lineScore*255, 0, 0);
+      strokeWeight(lineScore*10)
       line(p1.position.x, p1.position.y, p2.position.x, p2.position.y);
     });
   });
